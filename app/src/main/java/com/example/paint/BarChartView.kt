eb40e4f264871827eb5c438ca0d8465f01f87bce
Ahmet <ahmetturk.pc@gmail.com>
2019-12-21 14:41:25 +0300

    Draw axis lines

--- app/src/main/java/com/example/paint/BarChartView.kt
+++ app/src/main/java/com/example/paint/BarChartView.kt
@@ -1,10 +1,13 @@
 package com.example.paint
 
 import android.content.Context
+import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.content.ContextCompat
+import androidx.core.content.withStyledAttributes
 
 class BarChartView @JvmOverloads constructor(
     context: Context,
@@ -29,4 +32,26 @@ class BarChartView @JvmOverloads constructor(
         strokeWidth = resources.getDimension(R.dimen.bar_chart_line_width)
     }
 
+    private var padding = 0f
+    private var barSpace = 0f
+
+    private val rect = RectF()
+
+    init {
+        context.withStyledAttributes(attrs, R.styleable.BarChartView) {
+            padding = getDimension(R.styleable.BarChartView_android_padding, 0f)
+            barSpace = getDimension(R.styleable.BarChartView_android_spacing, 0f)
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        rect.set(padding, padding, width - padding, height - padding)
+
+        drawAxisLines(canvas)
+    }
+
+    private fun drawAxisLines(canvas: Canvas) {
+        canvas.drawLine(rect.left, rect.bottom, rect.left, rect.top, axisPaint)
+        canvas.drawLine(rect.left, rect.bottom, rect.right, rect.bottom, axisPaint)
+    }
 }
